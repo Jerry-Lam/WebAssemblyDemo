7113fafa462396c98167508d071e4cc52a44daf9
jerryLam <a17512049507@gmail.com>
2022-05-20 18:33:45 +0800

    初始化完成

---
+++ bulid/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../public/a.pdb"
+  "../public/a.wasm.js"
+  "CMakeFiles/a.dir/src/main.cpp.o"
+  "CMakeFiles/a.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
